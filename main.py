e75f0a45dce0035122a40377a4687b1de2b20804
VenkataDivya1485 <venkata.nallamothu@gisma-student.com>
2026-05-27 16:46:00 +0000

    python coding

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
 print ("Hello world!")
+
+def divide(a, b):
+    print("dividing", a, "by", b)
+    return a / b
+    print("result is", result)
+
+    return result
+
+try:
+    print("before calling divide")
+    divide(10, 0)
+    print("after calling divide")
+
+
+except ZeroDivisionError:
+    print("cannot divide by zero")
+except:
+    print("an error occured during division")
+else:
+    print("division successful")
+finally:
+    print("finished division operation")
+
+    
